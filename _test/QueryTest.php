a488d6bb88f5bd7ce504c9d517e4c0becf093568
Andreas Gohr <andi@splitbrain.org>
2023-01-17 13:25:51 +0100

    added test for URL parsing

---
+++ _test/QueryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace dokuwiki\plugin\dbquery\test;
+
+use DokuWikiTest;
+
+/**
+ * Query execution tests for the dbquery plugin
+ *
+ * @group plugin_dbquery
+ * @group plugins
+ */
+class QueryTest extends DokuWikiTest
+{
+
+    /**
+     * @see testUrlParsing
+     */
+    public function provideLinkContent()
+    {
+        return [
+            ['foo bar', '/^foo bar$/'],
+            ['[[]]', '/^\[\[\]\]$/'],
+            ['Nope [[wiki]]', '/^Nope \[\[wiki\]\]$/'],
+            ['[[wiki]]', '/data-wiki-id="wiki">wiki</'],
+            ['[[wiki|]]', '/data-wiki-id="wiki">wiki</'],
+            ['[[wiki|Test]]', '/data-wiki-id="wiki">Test</'],
+            ['[[http://foobar]]', '/href="http:\/\/foobar".*>.*foobar</'],
+            ['[[http://foobar|Test]]', '/href="http:\/\/foobar".*>Test</'],
+        ];
+    }
+
+    /**
+     * @dataProvider provideLinkContent
+     */
+    public function testUrlParsing($content, $expect)
+    {
+        $plugin = new \syntax_plugin_dbquery_query();
+        $R = new \Doku_Renderer_xhtml();
+
+        $this->callInaccessibleMethod($plugin, 'cellFormat', [$content, $R]);
+
+        $this->assertRegExp($expect, $R->doc);
+    }
+}
